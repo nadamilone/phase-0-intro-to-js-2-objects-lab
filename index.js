9918cf6887609111e3f6bccef440d1c1a86d032c
NADA MILONE <nadamilone@gmail.com>
2022-05-02 16:59:41 -0400

    commit tree 8aebda6eebe306326b4158d479fba75f91c39851

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Write your solution in this file!
+let employee = {};
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+  employee = {
+    name: `Sam`,
+    streetAddress: `11 Broadway`,
+  };
+  return employee;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+  employee[key] = value;
+  return employee;
+}
+destructivelyUpdateEmployeeWithKeyAndValue(employee, `name`, `Eli`);
+
+function deleteFromEmployeeByKey(employee, key) {
+  let { [key]: omit, ...res } = employee;
+  return res;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+  delete employee[key];
+  return employee;
+}
